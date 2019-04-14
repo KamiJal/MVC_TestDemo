8d04b8dd3f199520627c58763af661e42be694d4
Kamil <ushurbakiev.k@gmail.com>
2019-04-14 16:13:50 +0600

    added_Models.Product added_Utils.AppStrings

---
+++ TestDemo/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TestDemo.Utils;
+
+namespace TestDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = AppStrings.ErrorRequired)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = AppStrings.ErrorRequired)]
+        [Display(Name = AppStrings.DisplayNameManufacturer)]
+        public int ManufacturerId { get; set; }
+
+        [Required(ErrorMessage = AppStrings.ErrorRequired)]
+        [Display(Name = AppStrings.DisplayNameProductCode)]
+        public int ProductCode { get; set; }
+
+        [Required(ErrorMessage = AppStrings.ErrorRequired)]      
+        public decimal Price { get; set; }
+    }
+}
---
+++ TestDemo/Utils/AppStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestDemo.Utils
+{
+    public static class AppStrings
+    {
+        public const string ErrorRequired = "This field is required.";
+        public const string DisplayNameManufacturer = "Manufacturer";
+        public const string DisplayNameProductCode = "Product code";
+    }
+}
